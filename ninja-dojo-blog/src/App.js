ed2907bf3af1d8ab7f98219bd215814421821e83
Samir Azzouz <s.azzouz97@gmail.com>
2021-08-16 16:58:37 +0100

    Finishing tutorials

--- ninja-dojo-blog/src/App.js
+++ ninja-dojo-blog/src/App.js
@@ -1,20 +1,33 @@
-import logo from "./logo.svg";
-import Sample from "./components/Sample";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <Sample />
-    </div>
+    </Router>
   );
 }
 
